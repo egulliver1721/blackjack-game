8165aa55528d405f23ba0f72525f04132c1906f8
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2022-12-22 14:31:54 +0800

    updated functions

---
+++ index.js
@@ -0,0 +1,61 @@
+let cards = [];
+sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function start() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+
+  sumEl.textContent = `Sum: ${sum}`;
+  console.log(message);
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("drawing a new card from the deck!");
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  renderGame();
+  console.log(cards);
+  console.log(sum);
+
+  // sumEl.textContent = `Sum: ${newSum}`;
+}
